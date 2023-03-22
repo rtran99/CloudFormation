725c59213c64d07650e78e56fe606ac2f2b15582
rtran99 <62718771+rtran99@users.noreply.github.com>
2023-03-22 09:10:33 -0700

    Add files via upload

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,12 @@
+pipeline {
+  agent any
+  stages {
+    stage('Create CloudFormation Stack') {
+      steps {
+        script {
+          sh "aws cloudformation create-stack --stack-name Ronycloud --template-body file://ec2.yaml --region 'us-east-1'"
+        }
+      }
+    }
+  }
+}
